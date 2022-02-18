65f025ccfbb7e4fe913ef9687a93034d68892277
MeraWoss <92162315+MeraWoss@users.noreply.github.com>
2022-02-18 14:12:31 +0100

    Add files via upload

---
+++ ejercicios para clase/STYLE/style.js
@@ -0,0 +1,12 @@
+function cargarfoco(){
+    var campo = document.getElementById("texto");
+    campo.style.backgroundColor ="red";
+
+    
+}
+
+function descargarfoco(){
+    var campo = document.getElementById("texto");
+    campo.style.backgroundColor="green";
+    
+}
